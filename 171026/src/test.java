57ec3a396d4d3a08e234f5df7824b0e5550187ea
pc <pc@DESKTOP-F5SCT2A>
2017-10-26 14:43:23 +0900

    테스트 등록

---
+++ 171026/src/test.java
@@ -0,0 +1,6 @@
+//Git Hub에 올라가는 test하는 프로그램
+public class test {
+	public static void main(String args[]) {
+		System.out.println("test");
+	}
+}
---
+++ 171026/src/test_2.java
@@ -0,0 +1,6 @@
+
+public class test_2 {
+	public static void main(String args[]) {
+		System.out.println("개발프로젝트 강유빈");
+	}
+}
